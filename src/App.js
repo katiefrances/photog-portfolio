96099d6a37f6bba6eb35a6db6558089ca7c615ae
Katie Frances <katiebeeisdead@gmail.com>
2019-03-12 14:00:42 +1100

    Add navbar & add margin-top to fix viewport height

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 
 import Header from './Header.js';
 import './Header.css';
-// import Nav from './Nav.js';
-// import './Nav.css';
+import Nav from './Nav.js';
+import './Nav.css';
 import SectionOne from './SectionOne.js';
 import './SectionOne.css';
 import SectionTwo from './SectionTwo.js';
 import './SectionTwo.css';
-
+import 'react-flexbox-grid';
+import 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="Container">
+        <Nav></Nav>
           <Header></Header>
           <SectionOne></SectionOne>
           <SectionTwo></SectionTwo>
